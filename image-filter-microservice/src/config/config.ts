9b1907dd51b61b5647a6009215c67e00f26ab553
Daniel Tortosa Marangoni <danielmarangoni@yahoo.com.br>
2020-09-09 16:17:59 -0300

    feat: Implement auth middlewares and router

---
+++ image-filter-microservice/src/config/config.ts
@@ -0,0 +1,22 @@
+/**
+ * Server dev and prod environments variables configuration.
+ */
+const config = {
+    dev: {
+        restapi_client_id: process.env.RESTAPI_CLIENT_ID,
+        restapi_private_key: process.env.RESTAPI_PRIVATE_KEY,
+        restapi_host: process.env.RESTAPI_HOST,
+        img_jwt_secret: process.env.IMG_JWT_SECRET,
+    },
+    prod: {
+        restapi_client_id: '',
+        restapi_private_key: '',
+        restapi_host: '',
+        img_jwt_secret: '',
+    },
+};
+
+/**
+ * Current configuration working mode: dev or prod.
+ */
+export const currentConfig = config.dev;
--- image-filter-microservice/src/controllers/V0/index.router.ts
+++ image-filter-microservice/src/controllers/V0/index.router.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
 
 import { ImgFilterRouter } from './image-filter/routes/image-filter.router';
+import { authRouter } from './auth/routes/auth.router';
 
 const router: Router = Router();
 
+// Router filter for Auth Router
+router.use('/auth', authRouter);
+
+// Router filter for Image Filtering Router
 router.use('/filteredimage', ImgFilterRouter);
 
 // Send a description of this router root
