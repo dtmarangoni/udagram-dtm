0f67ef5a9f5265246848e86d209917ff60ad22fa
Daniel Tortosa Marangoni <danielmarangoni@yahoo.com.br>
2020-09-09 16:20:46 -0300

    feat: Add auth validation to image filter endpoint

--- image-filter-microservice/src/controllers/V0/image-filter/routes/image-filter.router.ts
+++ image-filter-microservice/src/controllers/V0/image-filter/routes/image-filter.router.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 
+import { validateAccessJWT, requireHeadersJWT } from '../../auth/middlewares/auth.middleware';
 import { filterImageFromURL, deleteLocalFiles } from '../../../../util/util';
 
 const router: Router = Router();
@@ -10,12 +11,12 @@ const router: Router = Router();
  * Get an image from a public url and apply predefined filters.
  * @returns The filtered image if ok otherwise an error response.
  */
-router.get('/', async (req: Request, res: Response) => {
+router.get('/', requireHeadersJWT, validateAccessJWT, async (req: Request, res: Response) => {
     // Retrieve the image url from a query parameter
     const image_url = req.query.image_url as string;
 
     // Sends an error response in case the query parameter is missing
-    if (!image_url) res.status(400).json({ error: { message: 'Missing public image URL to be filtered!' } });
+    if (!image_url) return res.status(400).json({ error: { message: 'Missing public image URL to be filtered!' } });
 
     // Filtered file path
     let filteredFilePath: string;
@@ -24,7 +25,7 @@ router.get('/', async (req: Request, res: Response) => {
         // Filter the image and gets it's saved local path
         filteredFilePath = await filterImageFromURL(image_url);
         // Send the filtered file in the response
-        res.status(200).sendFile(filteredFilePath, async (error) => {
+        return res.status(201).sendFile(filteredFilePath, async (error) => {
             if (error) {
                 // An error happened, rethrow it
                 throw new Error(`Error while sending the file: ${error.message}`);
@@ -34,7 +35,7 @@ router.get('/', async (req: Request, res: Response) => {
         });
     } catch (error) {
         // An error occurred, thus send the error response
-        res.status(500).json({ error: { message: error.message } });
+        return res.status(500).json({ error: { message: error.message } });
     }
 });
 
