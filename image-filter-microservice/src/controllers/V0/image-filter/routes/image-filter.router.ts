480363a04338f7c38d7c973d04013a14966190af
Daniel Tortosa Marangoni <danielmarangoni@yahoo.com.br>
2020-09-08 23:00:27 -0300

    feat: Implement image filtering route

--- image-filter-microservice/src/controllers/V0/image-filter/routes/image-filter.router.ts
+++ image-filter-microservice/src/controllers/V0/image-filter/routes/image-filter.router.ts
@@ -1,24 +1,42 @@
 import { Router, Request, Response } from 'express';
 
+import { filterImageFromURL, deleteLocalFiles } from '../../../../util/util';
+
 const router: Router = Router();
 
-// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-// GET /filteredimage?image_url={{URL}}
-// endpoint to filter an image from a public url.
-// IT SHOULD
-//    1
-//    1. validate the image_url query
-//    2. call filterImageFromURL(image_url) to filter the image
-//    3. send the resulting file in the response
-//    4. deletes any files on the server on finish of the response
-// QUERY PARAMATERS
-//    image_url: URL of a publicly accessible image
-// RETURNS
-//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+/**
+ * Route: http://{{host}}/api/v0/filteredimage?image_url={{public image url}}
+ *
+ * Get an image from a public url and apply predefined filters.
+ * @returns The filtered image if ok otherwise an error response.
+ */
+router.get('/', async (req: Request, res: Response) => {
+    // Retrieve the image url from a query parameter
+    const image_url = req.query.image_url as string;
 
-/**************************************************************************** */
+    // Sends an error response in case the query parameter is missing
+    if (!image_url) res.status(400).json({ error: { message: 'Missing public image URL to be filtered!' } });
 
-//! END @TODO1
+    // Filtered file path
+    let filteredFilePath: string;
+
+    try {
+        // Filter the image and gets it's saved local path
+        filteredFilePath = await filterImageFromURL(image_url);
+        // Send the filtered file in the response
+        res.status(200).sendFile(filteredFilePath, async (error) => {
+            if (error) {
+                // An error happened, rethrow it
+                throw new Error(`Error while sending the file: ${error.message}`);
+            }
+            // After file transfer finishes, delete it from tmp folder
+            await deleteLocalFiles([filteredFilePath]);
+        });
+    } catch (error) {
+        // An error occurred, thus send the error response
+        res.status(500).json({ error: { message: error.message } });
+    }
+});
 
 // The image filter router
 export const ImgFilterRouter: Router = router;
--- image-filter-microservice/src/server.ts
+++ image-filter-microservice/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import { filterImageFromURL, deleteLocalFiles } from './util/util';
 import { IndexRouter } from './controllers/V0/index.router';
 
 // The server main entrance function
