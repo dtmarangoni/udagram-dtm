3ef4e04dcd94b41ad1349c9591d54e40b9f667d2
Daniel Tortosa Marangoni <danielmarangoni@yahoo.com.br>
2020-09-12 16:18:22 -0300

    feat: Add REST API call to img microservice

--- backend-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ backend-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express';
 import { FeedItem } from '../models/FeedItem';
+
 import { requireAuth } from '../../users/routes/auth.router';
+import { filterImage, requireFeedData } from '../middlewares/feed.middleware';
 import * as AWS from '../../../../aws';
 
 const router: Router = Router();
@@ -18,28 +20,28 @@ router.get('/', async (req: Request, res: Response) => {
     });
 
     // Respond with fetched items
-    res.status(200).send(items);
+    return res.status(200).send(items);
 });
 
 // Get a specific resource by Primary Key
 router.get('/:id', async (req: Request, res: Response) => {
     const item = await FeedItem.findByPk(req.params.id);
     // No item in DB for the given ID
-    if (!item) res.status(404).json({ error: { message: 'No feed found for this ID.' } });
+    if (!item) return res.status(404).json({ error: { message: 'No feed found for this ID.' } });
 
     // Get from AWS S3 a signed url to user have direct
     // access to the object
     item.url = AWS.getGetSignedUrl(item.url);
 
     // Respond with the found feed item
-    res.status(200).json(item);
+    return res.status(200).json(item);
 });
 
 // Update a specific resource
 router.patch('/:id', requireAuth, async (req: Request, res: Response) => {
     const item = await FeedItem.findByPk(req.params.id);
     // No item found for the given ID
-    if (!item) res.status(404).json({ error: { message: 'No feed found for this ID.' } });
+    if (!item) return res.status(404).json({ error: { message: 'No feed found for this ID.' } });
 
     // Update the feed item according to the given fields
     if (req.body.caption) item.caption = req.body.caption;
@@ -52,7 +54,7 @@ router.patch('/:id', requireAuth, async (req: Request, res: Response) => {
     item.url = AWS.getGetSignedUrl(item.url);
 
     // Respond with the updated item
-    res.status(200).json(item);
+    return res.status(200).json(item);
 });
 
 // Get a signed url to user be able to put a new item in the bucket
@@ -61,27 +63,18 @@ router.get('/signed-url/:fileName', requireAuth, async (req: Request, res: Respo
     // Get the signed url
     const url = AWS.getPutSignedUrl(fileName);
     // Respond with the signed url
-    res.status(201).send({ url: url });
+    return res.status(201).send({ url: url });
 });
 
 // Post meta data and the filename after a file is uploaded
 // NOTE the file name is they key name in AWS S3 bucket.
 // body : {caption: string, fileName: string};
-router.post('/', requireAuth, async (req: Request, res: Response) => {
-    const caption = req.body.caption;
-    const fileName = req.body.url;
+router.post('/', requireAuth, requireFeedData, filterImage, async (req: Request, res: Response) => {
+    // Get the feed data from request
+    const caption = req.caption;
+    const fileName = req.fileName;
 
-    // Check if the caption is valid
-    if (!caption) {
-        return res.status(400).json({ error: { message: 'Caption is required or malformed' } });
-    }
-
-    // Check if the filename is valid
-    if (!fileName) {
-        return res.status(400).send({ error: { message: 'File url is required' } });
-    }
-
-    // Create the new feed item
+    // Create the new DB feed item
     const item = new FeedItem({
         caption: caption,
         url: fileName,
@@ -90,12 +83,16 @@ router.post('/', requireAuth, async (req: Request, res: Response) => {
     // Save the feed item in DB
     const saved_item = await item.save();
 
-    // Get from AWS S3 a signed url to user have direct
+    // Get from AWS S3 a public signed url to frontend have direct
     // access to the object
-    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+    const filePublicUrl = AWS.getGetSignedUrl(fileName);
+
+    // Update the file name to the public url before sending the data
+    // back to frontend
+    saved_item.url = filePublicUrl;
 
     // Respond with the created and saved feed item
-    res.status(201).send(saved_item);
+    return res.status(201).send(saved_item);
 });
 
 // The feed router
